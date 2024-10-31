62dbeb645530c254a471fa685491c1b6a34f1019
Andrej Orsula <orsula.andrej@gmail.com>
2024-10-31 11:52:14 +0100

    Update rendering settings - Adjust app renderer settings - Add utils for setting RTX rendering settings (post-processing) - Enable simple fog alongside Mars sky to better simulate suspended dust
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- space_robotics_bench/assets/light/sky.py
+++ space_robotics_bench/assets/light/sky.py
@@ -7,6 +7,7 @@ import space_robotics_bench.core.envs as env_utils
 import space_robotics_bench.core.sim as sim_utils
 from space_robotics_bench.core.assets import AssetBaseCfg
 from space_robotics_bench.paths import SRB_ASSETS_DIR_SRB_HDRI
+from space_robotics_bench.utils import rtx_settings
 
 
 def sky_from_env_cfg(
@@ -24,6 +25,13 @@ def sky_from_env_cfg(
                 f"{ISAAC_NUCLEUS_DIR}/Materials/Textures/Skies/PolyHaven/kloofendal_43d_clear_puresky_4k.hdr",
             )
         case env_utils.Scenario.MARS:
+            rtx_settings.simple_fog(
+                color=(0.8, 0.4, 0.2),
+                intensity=0.25,
+                start_height=16.0,
+                height_density=0.5,
+                fog_distance_density=0.05,
+            )
             texture_file = path.join(SRB_ASSETS_DIR_SRB_HDRI, "martian_sky_day.hdr")
         case env_utils.Scenario.ORBIT:
             texture_file = path.join(SRB_ASSETS_DIR_SRB_HDRI, "low_lunar_orbit.jpg")
