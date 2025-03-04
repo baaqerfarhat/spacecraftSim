119fb84dec69270ec06882437cbd7a5d83a08372
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-04 17:49:25 +0100

    Add extra shape spawners
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/core/sim/spawners/__init__.py
+++ srb/core/sim/spawners/__init__.py
@@ -3,3 +3,10 @@ from simforge.integrations.isaaclab.spawner import (  # noqa: F401
     SimforgeAssetCfg,
     UsdFileCfg,
 )
+
+from .shapes import (  # noqa: F401
+    ArrowCfg,
+    MultiShapeSpawnerCfg,
+    PinnedArrowCfg,
+    PinnedSphereCfg,
+)
