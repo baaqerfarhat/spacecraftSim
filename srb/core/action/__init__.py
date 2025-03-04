d7095a6de15102f1c5ab4ec498b4ef26effd3c02
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-04 17:45:59 +0100

    Define ActionGroup model
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ srb/core/action/__init__.py
@@ -0,0 +1,4 @@
+from isaaclab.envs.mdp.actions import *  # noqa: F403
+from isaaclab.envs.mdp.actions.task_space_actions import *  # noqa: F403
+
+from .action_group import ActionGroup, ActionGroupRegistry  # noqa: F401
