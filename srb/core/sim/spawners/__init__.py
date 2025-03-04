53b53d9f8b7a193fb18fa610ea41bf33eed3540f
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-04 17:52:52 +0100

    Add ParticleSpawner
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/core/sim/spawners/__init__.py
+++ srb/core/sim/spawners/__init__.py
@@ -4,6 +4,15 @@ from simforge.integrations.isaaclab.spawner import (  # noqa: F401
     UsdFileCfg,
 )
 
+from .particles import (  # noqa: F401
+    GridParticlesSpawnerCfg,
+    ParticlesSpawnerCfg,
+    PyramidParticlesSpawnerCfg,
+    particle_positions,
+    particle_velocities,
+    set_particle_positions,
+    set_particle_velocities,
+)
 from .shapes import (  # noqa: F401
     ArrowCfg,
     MultiShapeSpawnerCfg,
