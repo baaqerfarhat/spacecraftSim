92e4f7d44235816eebe02b9d642340fe66285cc7
Andrej Orsula <orsula.andrej@gmail.com>
2025-03-20 01:06:19 +0100

    Remove redundant event
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/core/mdp/events.py
+++ srb/core/mdp/events.py
@@ -88,46 +88,6 @@ def reset_xform_orientation_uniform(
     asset.set_world_poses(orientations=orientations)
 
 
-def reset_joints_by_offset(
-    env: "AnyEnv",
-    env_ids: torch.Tensor,
-    position_range: tuple[float, float],
-    velocity_range: tuple[float, float],
-    asset_cfg: SceneEntityCfg,
-):
-    """Reset the robot joints with offsets around the default position and velocity by the given ranges.
-
-    This function samples random values from the given ranges and biases the default joint positions and velocities
-    by these values. The biased values are then set into the physics simulation.
-    """
-    # Extract the used quantities (to enable type-hinting)
-    asset: Articulation = env.scene[asset_cfg.name]
-
-    # Get default joint state
-    joint_pos = asset.data.default_joint_pos[env_ids].clone()
-    joint_vel = asset.data.default_joint_vel[env_ids].clone()
-
-    # Bias these values randomly
-    joint_pos += sample_uniform(*position_range, joint_pos.shape, joint_pos.device)  # type: ignore
-    joint_vel += sample_uniform(*velocity_range, joint_vel.shape, joint_vel.device)  # type: ignore
-
-    # Clamp joint pos to limits
-    joint_pos_limits = asset.data.soft_joint_pos_limits[env_ids]
-    joint_pos = joint_pos.clamp_(joint_pos_limits[..., 0], joint_pos_limits[..., 1])
-    # Clamp joint vel to limits
-    joint_vel_limits = asset.data.soft_joint_vel_limits[env_ids]
-    joint_vel = joint_vel.clamp_(-joint_vel_limits, joint_vel_limits)
-
-    # Set into the physics simulation
-    joint_indices = asset.find_joints(asset_cfg.joint_names)[0]  # type: ignore
-    asset.write_joint_state_to_sim(
-        joint_pos[:, joint_indices],
-        joint_vel[:, joint_indices],
-        joint_ids=joint_indices,
-        env_ids=env_ids,  # type: ignore
-    )
-
-
 def randomize_usd_prim_attribute_uniform(
     env: "AnyEnv",
     env_ids: torch.Tensor | None,
