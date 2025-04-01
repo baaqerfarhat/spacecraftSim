51442431629760fd669d3451ebfb0cba338ec83f
Andrej Orsula <orsula.andrej@gmail.com>
2025-04-01 23:15:25 +0200

    Tune peg_in_hole rewards
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- srb/tasks/manipulation/peg_in_hole_assembly/task.py
+++ srb/tasks/manipulation/peg_in_hole_assembly/task.py
@@ -176,6 +176,8 @@ class Task(ManipulationEnv):
                 )
                 else None
             ),
+            joint_acc_robot=self._robot.data.joint_acc,
+            joint_applied_torque_robot=self._robot.data.applied_torque,
             # Kinematics
             fk_pos_end_effector=self._tf_end_effector.data.target_pos_source[:, 0, :],
             fk_quat_end_effector=self._tf_end_effector.data.target_quat_source[:, 0, :],
@@ -219,6 +221,8 @@ def _compute_step_return(
     joint_pos_limits_robot: torch.Tensor | None,
     joint_pos_end_effector: torch.Tensor | None,
     joint_pos_limits_end_effector: torch.Tensor | None,
+    joint_acc_robot: torch.Tensor,
+    joint_applied_torque_robot: torch.Tensor,
     # Kinematics
     fk_pos_end_effector: torch.Tensor,
     fk_quat_end_effector: torch.Tensor,
@@ -371,17 +375,48 @@ def _compute_step_return(
         torch.square(act_current - act_previous), dim=1
     )
 
+    # Penalty: Joint torque
+    WEIGHT_JOINT_TORQUE = -0.000025
+    MAX_JOINT_TORQUE_PENALTY = -4.0
+    penalty_joint_torque = torch.clamp_min(
+        WEIGHT_JOINT_TORQUE
+        * torch.sum(torch.square(joint_applied_torque_robot), dim=1),
+        min=MAX_JOINT_TORQUE_PENALTY,
+    )
+
+    # Penalty: Joint acceleration
+    WEIGHT_JOINT_ACCELERATION = -0.0005
+    MAX_JOINT_ACCELERATION_PENALTY = -4.0
+    penalty_joint_acceleration = torch.clamp_min(
+        WEIGHT_JOINT_ACCELERATION * torch.sum(torch.square(joint_acc_robot), dim=1),
+        min=MAX_JOINT_ACCELERATION_PENALTY,
+    )
+
     # Penalty: Undesired robot contacts
-    WEIGHT_UNDESIRED_ROBOT_CONTACTS = -0.1
+    WEIGHT_UNDESIRED_ROBOT_CONTACTS = -10.0
     THRESHOLD_UNDESIRED_ROBOT_CONTACTS = 10.0
     penalty_undesired_robot_contacts = WEIGHT_UNDESIRED_ROBOT_CONTACTS * (
         torch.max(torch.norm(contact_forces_robot, dim=-1), dim=1)[0]
         > THRESHOLD_UNDESIRED_ROBOT_CONTACTS
     )
 
+    # Reward: End-effector top-down orientation
+    WEIGHT_TOP_DOWN_ORIENTATION = 5.0
+    TANH_STD_TOP_DOWN_ORIENTATION = 0.15
+    top_down_alignment = torch.sum(
+        fk_rotmat_end_effector[:, :, 2]
+        * torch.tensor((0.0, 0.0, -1.0), device=device)
+        .unsqueeze(0)
+        .expand(num_envs, 3),
+        dim=1,
+    )
+    reward_top_down_orientation = WEIGHT_TOP_DOWN_ORIENTATION * (
+        1.0 - torch.tanh((1.0 - top_down_alignment) / TANH_STD_TOP_DOWN_ORIENTATION)
+    )
+
     # Reward: Distance | End-effector <--> Object
-    WEIGHT_DISTANCE_END_EFFECTOR_TO_OBJ = 1.0
-    TANH_STD_DISTANCE_END_EFFECTOR_TO_OBJ = 0.25
+    WEIGHT_DISTANCE_END_EFFECTOR_TO_OBJ = 2.5
+    TANH_STD_DISTANCE_END_EFFECTOR_TO_OBJ = 0.2
     reward_distance_end_effector_to_obj = WEIGHT_DISTANCE_END_EFFECTOR_TO_OBJ * (
         1.0
         - torch.tanh(
@@ -391,7 +426,7 @@ def _compute_step_return(
     )
 
     # Reward: Grasp object
-    WEIGHT_GRASP = 4.0
+    WEIGHT_GRASP = 8.0
     THRESHOLD_GRASP = 5.0
     reward_grasp = (
         WEIGHT_GRASP
@@ -409,10 +444,10 @@ def _compute_step_return(
     )
 
     # Reward: Lift object
-    WEIGHT_LIFT = 16.0
-    HEIGHT_OFFSET_LIFT = 0.5
-    HEIGHT_SPAN_LIFT = 0.25
-    TANH_STD_HEIGHT_LIFT = 0.1
+    WEIGHT_LIFT = 4.0
+    HEIGHT_OFFSET_LIFT = 0.2
+    HEIGHT_SPAN_LIFT = 0.1
+    TANH_STD_HEIGHT_LIFT = 0.05
     reward_lift = WEIGHT_LIFT * (
         1.0
         - torch.tanh(
@@ -467,9 +502,25 @@ def _compute_step_return(
         )
     )
 
+    # Reward: Distance | Peg -> Hole entrance (gradual)
+    WEIGHT_DISTANCE_PEG_TO_HOLE_ENTRANCE_GRADUAL = 8.0
+    TANH_STD_DISTANCE_PEG_TO_HOLE_ENTRANCE_GRADUAL = 0.16
+    reward_distance_peg_to_hole_entrance_gradual = (
+        WEIGHT_DISTANCE_PEG_TO_HOLE_ENTRANCE_GRADUAL
+        * (
+            1.0
+            - torch.tanh(
+                torch.min(torch.norm(tf_pos_peg_ends_to_hole_entrance, dim=-1), dim=1)[
+                    0
+                ]
+                / TANH_STD_DISTANCE_PEG_TO_HOLE_ENTRANCE_GRADUAL
+            )
+        )
+    )
+
     # Reward: Distance | Peg -> Hole entrance
-    WEIGHT_DISTANCE_PEG_TO_HOLE_ENTRANCE = 16.0
-    TANH_STD_DISTANCE_PEG_TO_HOLE_ENTRANCE = 0.05
+    WEIGHT_DISTANCE_PEG_TO_HOLE_ENTRANCE = 32.0
+    TANH_STD_DISTANCE_PEG_TO_HOLE_ENTRANCE = 0.04
     reward_distance_peg_to_hole_entrance = WEIGHT_DISTANCE_PEG_TO_HOLE_ENTRANCE * (
         1.0
         - torch.tanh(
@@ -479,8 +530,8 @@ def _compute_step_return(
     )
 
     # Reward: Distance | Peg -> Hole bottom
-    WEIGHT_DISTANCE_PEG_TO_HOLE_BOTTOM = 128.0
-    TANH_STD_DISTANCE_PEG_TO_HOLE_BOTTOM = 0.005
+    WEIGHT_DISTANCE_PEG_TO_HOLE_BOTTOM = 256.0
+    TANH_STD_DISTANCE_PEG_TO_HOLE_BOTTOM = 0.002
     reward_distance_peg_to_hole_bottom = WEIGHT_DISTANCE_PEG_TO_HOLE_BOTTOM * (
         1.0
         - torch.tanh(
@@ -525,16 +576,22 @@ def _compute_step_return(
             "proprio_dyn": {
                 "joint_pos_robot_normalized": joint_pos_robot_normalized,
                 "joint_pos_end_effector_normalized": joint_pos_end_effector_normalized,
+                "joint_acc_robot": joint_acc_robot,
+                "joint_applied_torque_robot": joint_applied_torque_robot,
             },
         },
         {
             "penalty_action_rate": penalty_action_rate,
+            "penalty_joint_torque": penalty_joint_torque,
+            "penalty_joint_acceleration": penalty_joint_acceleration,
             "penalty_undesired_robot_contacts": penalty_undesired_robot_contacts,
+            "reward_top_down_orientation": reward_top_down_orientation,
             "reward_distance_end_effector_to_obj": reward_distance_end_effector_to_obj,
             "reward_grasp": reward_grasp,
             "reward_lift": reward_lift,
             "reward_align_peg_to_hole_primary": reward_align_peg_to_hole_primary,
             "reward_align_peg_to_hole_secondary": reward_align_peg_to_hole_secondary,
+            "reward_distance_peg_to_hole_entrance_gradual": reward_distance_peg_to_hole_entrance_gradual,
             "reward_distance_peg_to_hole_entrance": reward_distance_peg_to_hole_entrance,
             "reward_distance_peg_to_hole_bottom": reward_distance_peg_to_hole_bottom,
         },
