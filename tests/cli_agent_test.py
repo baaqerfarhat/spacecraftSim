b24ce00d1f7c98b0132a0ec5603ef593247f9868
Andrej Orsula <orsula.andrej@gmail.com>
2025-06-04 18:24:05 +0200

    test: Add workflow for performance evaluation
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- tests/cli_agent_test.py
+++ tests/cli_agent_test.py
@@ -8,7 +8,7 @@ from typing import Iterable
 import pytest
 
 from srb.utils import logging
-from srb.utils.cache import read_env_list_cache
+from srb.utils.cache import read_offline_srb_env_cache
 from srb.utils.isaacsim import get_isaacsim_python
 from srb.utils.subprocess import terminate_process
 
@@ -17,13 +17,28 @@ MAX_DURATION_RAND: float = 40.0
 MAX_DURATION_TRAIN: float = 80.0
 
 HEADLESS: bool = True
-TEST_VISUAL_ENVS: bool = True
+STACK: bool = True
+TEST_VISUAL_ENVS: bool = False
+TEST_TEMPLATE_ENVS: bool = False
+TEST_EXCAVATION: bool = True
+
+PERF: bool = False
+PERF_DURATION: float = 600.0
+PERF_OUTPUT_DIR: str = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs", "_perf"
+)
+
+TEST_START_TIME: float = time.time()
 
 
 def _list_envs() -> Iterable[str]:
-    if envs := read_env_list_cache():
+    if envs := read_offline_srb_env_cache():
         if not TEST_VISUAL_ENVS:
             envs = filter(lambda env: not _is_visual_env(env), envs)
+        if not TEST_TEMPLATE_ENVS:
+            envs = filter(lambda env: not env.startswith("_"), envs)
+        if not TEST_EXCAVATION:
+            envs = filter(lambda env: "excavation" not in env, envs)
         envs = sorted(envs)
     else:
         test_filepath = Path(__file__)
@@ -57,7 +72,28 @@ def test_cli_agent_rand(env: str, num_envs: int):
             env,
             "--logs",
             tmpdir,
+            f"env.stack={STACK}",
         )
+
+        if PERF:
+            cmd += (
+                "--perf",
+                "--perf_duration",
+                str(PERF_DURATION),
+                "--perf_output",
+                os.path.join(
+                    PERF_OUTPUT_DIR,
+                    f"perf_{env.replace('/', '_')}_{num_envs}_{int(TEST_START_TIME)}.txt",
+                ),
+            )
+
+            if not os.path.exists(PERF_OUTPUT_DIR):
+                os.makedirs(PERF_OUTPUT_DIR, exist_ok=True)
+            with open(
+                os.path.join(PERF_OUTPUT_DIR, f"commands_{int(TEST_START_TIME)}.bash"),
+                "a",
+            ) as f:
+                f.write(" ".join(cmd) + "\n")
 
         environ = os.environ.copy()
         environ["SF_BAKER"] = "0"
@@ -73,15 +109,18 @@ def test_cli_agent_rand(env: str, num_envs: int):
                 preexec_fn=os.setsid,
             )
 
-            start = time.time()
-            while time.time() - start < MAX_DURATION_RAND:
-                if process.poll() is not None:
-                    logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
-                    stdout, stderr = process.communicate()
-                    pytest.fail(
-                        f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
-                    )
-                time.sleep(0.01 * MAX_DURATION_RAND)
+            if PERF:
+                process.wait()
+            else:
+                start = time.time()
+                while time.time() - start < MAX_DURATION_RAND:
+                    if process.poll() is not None:
+                        logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
+                        stdout, stderr = process.communicate()
+                        pytest.fail(
+                            f'Process failed for env "{env}"\n[env={env}] STDOUT:\n{stdout}\n[env={env}] STDERR:\n{stderr}'
+                        )
+                    time.sleep(0.01 * MAX_DURATION_RAND)
         except Exception as e:
             logging.critical(f"[{env}] Failed command: {' '.join(cmd)}")
             pytest.fail(
